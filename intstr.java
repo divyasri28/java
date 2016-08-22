113dc64450ab07860fbee6c6f67fe7cf90250af6
divyasri28 <divyasrispd@gmail.com>
2016-08-22 16:04:47 +0530

    Add files via upload

---
+++ intstr.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class intstr{
+
+	
+	public static void main(String[] args){
+		Scanner in=new Scanner(System.in);
+		String s="98657";
+		int a=Integer.parseInt(s);
+		
+		System.out.println(a);
+		
+		
+		
+	    
+	}
+
+}
